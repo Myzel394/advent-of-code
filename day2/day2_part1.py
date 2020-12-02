9a633d0a48feb15823d0c60101cfa14cec51506b
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2020-12-02 19:08:33 +0100

    Update day2_part1.py
    Improved code readability (PEP); Removed unnecessary docs; Refactored names for easier understanding; Removed unnecessary if/else in `main`; Removed unnecessary `invalid` variable.

--- day2/day2_part1.py
+++ day2/day2_part1.py
@@ -1,46 +1,39 @@
 import os
+from pathlib import Path
+
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(THIS_FOLDER, "passwords"), "r") as file:
-    passwords = file.read().split("\n")
-    
-def password_work(password:str)->dict:
-    """
-    {
-        'raw': '3-4 t: tttt',
-        'char': 't',
-        'pass_wd': 'tttt',
-        'range': {
-            'min': 3, 
-            'max': 4
-            }
-    }
-    """
+
+def parse_password(password: str) -> dict:
     return_dict = {}
     return_dict["char"] = password.split(":")[0].split(" ")[1]
     return_dict["pass_wd"] = password.split(":")[1].strip()
     char_range = password.split(":")[0].split(" ")[0].split("-")
-    return_dict["range"] = {"min":int(char_range[0]),"max":int(char_range[1])}
+    return_dict["range"] = {"min": int(char_range[0]), "max": int(char_range[1])}
     return return_dict
 
-def validity_check(password_dict:dict)->bool:
-    """
-    True/False
-    """
+
+def is_password_valid(password_dict: dict) -> bool:
     if password_dict["char"] not in password_dict["pass_wd"]:
         return False
-    elif password_dict["pass_wd"].count(password_dict["char"]) == password_dict["range"]["min"] or password_dict["pass_wd"].count(password_dict["char"]) == password_dict["range"]["max"]:
+    elif password_dict["pass_wd"].count(password_dict["char"]) == password_dict["range"]["min"] \
+            or password_dict["pass_wd"].count(password_dict["char"]) == password_dict["range"]["max"]:
         return True
     elif not password_dict["range"]["min"] < password_dict["pass_wd"].count(password_dict["char"]) < password_dict["range"]["max"]:
         return False
     else:
         return True
 
-invalid = 0
-valid = 0
-for password in passwords:
-    if validity_check(password_work(password)):
-        valid += 1
-    else:
-        invalid += 1
-print(f"valid passwords: {valid}\ninvalid passwords: {invalid}")
+
+def main():
+    passwords = Path(THIS_FOLDER).joinpath("input.txt").read_text().splitlines()
+    
+    valid = 0
+    for password in passwords:
+        valid += int(is_password_valid(parse_password(password)))
+        
+    print(f"valid passwords: {valid}\ninvalid passwords: {len(passwords) - valid}")
+
+
+if __name__ == "__main__":
+    main()
